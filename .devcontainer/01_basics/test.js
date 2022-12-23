1ffc0d322b12a78f1e3f4a3746845b904d8bae99
rohit yadav <59434852+Rohityadavclg@users.noreply.github.com>
2022-12-23 14:01:26 +0000

    saved

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,3 +1,5 @@
 console.log("rohit");
 console.log("yadav");
 console.log("thank you sir");
+console.log("log on your github account. Goto codespace create new setup online machine");
+console.log("same like node js -> after performing operation -> save to git hub");
