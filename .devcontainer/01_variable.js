074541dc77b2fad2529552082395c5c9bb62f572
rohit yadav <59434852+Rohityadavclg@users.noreply.github.com>
2022-12-28 14:36:33 +0000

    datatypes

--- .devcontainer/01_variable.js
+++ .devcontainer/01_variable.js
@@ -2,5 +2,15 @@
  let acountEmail="rogj@gmail.com";
  var accountPassword="12345";
  accoutCity="Mumbai";
- accountId=2
+//  accountId=2
+acountEmail="rohit@132"
+let accountSate
+accountPassword="321"
+accoutCity="delhi"
  console.log(accountId);
+ /*
+ prefer not to use var 
+ because of issue in the block scope and functional scope
+ 
+ */
+ console.table([accountId,accountPassword,accoutCity,acountEmail,accountSate])
---
+++ .devcontainer/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";//treat all JS code as newer version
+
+//alert("hello")  we are using node js not browser
+// code should be high readable
+let name="rohit yadav"
+let age=19
+let isLoggedIN=false;
+//number->2 to power53
+//big int
+//null-> is standalone value// null is object 
+//boolean-> true/false
+//undefined=>
+//symbol=> unique
+//object
+console.log(typeof "rohit");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
