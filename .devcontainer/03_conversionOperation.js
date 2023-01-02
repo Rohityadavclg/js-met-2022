391f7716815b546a502b68f98a7e6d6c7a5488c8
rohit yadav <59434852+Rohityadavclg@users.noreply.github.com>
2023-01-02 13:28:54 +0000

    Data type conversion

---
+++ .devcontainer/03_conversionOperation.js
@@ -0,0 +1,25 @@
+//let score="33abs";
+//let score =null;
+//let score =undefined;
+//let score =true;
+//let score ="rohit";
+//const{score}=req.body;
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueInNumber=Number(score);
+//console.log(typeof score);
+//console.log(valueInNumber);
+//"33"=>33
+//"33abc"=Nan
+//true=>1 false=>0
+//let isLoggedIN=1
+//let isLoggedIN=""
+let isLoggedIN="rohti"
+let booleanIsLogedIn=Boolean(isLoggedIN)
+//console.log(booleanIsLogedIn);
+//1=>true 0=>false
+//""=> flase =>"dkff"=>"gofffj"
+let someNumber=33;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
